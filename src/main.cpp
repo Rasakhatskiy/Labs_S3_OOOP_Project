8b7fc3c38f3825b99a35c0e554784c23b540e9e0
Rasakhatskiy <saharok.m@yandex.ua>
2020-10-07 21:27:25 +0300

    Dark style + stylesheet loading

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,44 @@
 
 #include <QApplication>
 #include <QFile>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    //w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     w.show();
 
-    QFile styleFile("://forms/stylesheet.qss");
+    QFile styleFile(":/res/forms/stylesheet.qss");
     styleFile.open(QFile::ReadOnly);
-    a.setStyleSheet(QString::fromLatin1(styleFile.readAll()));
+    auto qstr = QString::fromLatin1(styleFile.readAll());
+    a.setStyleSheet(qstr);
     styleFile.close();
+
+    std::cout << qstr.toStdString();
 
     return a.exec();
 }
+/*
+ * #include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+ *
+ */
